dde1f1e1e938903361feb27135364e47d6ca1a5a
janderson1097 <jandersonbezerra1017@gmail.com>
2025-04-17 11:41:50 -0300

    Add files via upload

---
+++ trabalho janderson.py
@@ -0,0 +1,64 @@
+print("ola mundo")
+#1
+nome = "janderson"
+print(nome)
+#2
+nome = "janderson"
+curso =" lic computação"
+print(nome)
+print(curso)
+#3
+nome ="janderson"
+idade = 27
+print(nome)
+print(idade)
+#4
+a = 3
+b = 5
+resultado = 3*5
+print(resultado)
+#5
+num1 = 10
+num2 = 5
+num3 = 8
+media = (num1+num2+num3)/3
+print(media)
+#6
+salario = 1050
+imposto = salario*0.20
+print(imposto)
+#7
+base = 7
+altura = 14
+area = (base*altura)/2
+print(area)
+#8
+nota1 = 9.5
+nota2 = 7
+nota3 = 7.5
+media =(nota1+nota2+nota3)/3
+print(media)
+#9
+salario_bruto = 2200
+imposto =salario_bruto*0.15
+salario_liquido = salario_bruto-imposto
+print(salario_liquido)
+#10
+salario_bruto = 5000
+inss = salario_bruto*0.15
+ir = salario_bruto*0.10
+salario_liquido = salario_bruto-inss-ir
+print(salario_bruto)
+print(inss)
+print(ir)
+print(salario_liquido)
+#11
+raio = 4.5
+area = 3.14*raio**2
+print(area)
+#12
+a = 3.5
+b = 7.5
+media = (a*3.5+b*7.5)/11
+print(media)
+
